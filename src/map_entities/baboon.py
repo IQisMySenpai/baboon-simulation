659ff4998118d50beae7a4695d188f78bfbfa1db
Jannick <jannick.schroer@gmail.com>
2025-04-11 13:28:46 +0200

    Built a baboon class on Point2d

---
+++ src/map_entities/baboon.py
@@ -0,0 +1,22 @@
+from map_entities.point2d import Point2D
+
+class Baboon (Point2D):
+    """
+    A class representing a baboon in a 2D space.
+    Inherits from Point2D to leverage its functionalities.
+    """
+
+    def __init__(self, x: float, y: float, color: str = "green"):
+        """
+        Initialize a Baboon object.
+        :param x: X coordinate
+        :param y: Y coordinate
+        :param color: Color of the baboon (default is "green")
+        """
+        super().__init__(x, y, color)
+
+    def __str__(self):
+        return f"Baboon({self[0]}, {self[1]}, color={self.color})"
+
+    def __repr__(self):
+        return f"Baboon({self[0]}, {self[1]}, color={self.color})"
