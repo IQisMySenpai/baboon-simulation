b1e85e48043a978e3d3b776be06a9d6fddcec9e4
Pablo Cuesta Sierra <pablocsaa@gmail.com>
2025-04-14 12:50:21 +0200

    Style proposal

--- src/map_entities/baboon.py
+++ src/map_entities/baboon.py
@@ -1,23 +1,21 @@
-from map_entities.point2d import Point2D
+"""
+We are going to use weak, non-interpreter-enforced protocols to define objects.
+Further down the line we could eventually use
+https://peps.python.org/pep-0544/, but objects right now are too simple to
+require other machinery such as classes.
+Refer to https://peps.python.org/pep-0020/: "Simple is better than complex."
 
+Here is the documentation for said objects.
 
-class Baboon(Point2D):
-    """
-    A class representing a baboon in a 2D space.
-    Inherits from Point2D to leverage its functionalities.
-    """
+baboons: np.ndarray: (M, 2)-np.ndarray (i.e. Mx2 matrix)
+    - M is the number of baboons.
+    - 2 is the x and y coordinates of the baboon.
 
-    def __init__(self, x: float, y: float, color: str = "green"):
-        """
-        Initialize a Baboon object.
-        :param x: X coordinate
-        :param y: Y coordinate
-        :param color: Color of the baboon (default is "green")
-        """
-        super().__init__(x, y, color)
+baboons_trajectory: (N, M, 2)-np.ndarray (i.e. NxMx2 matrix)
+    - N is the number of steps.
+    - M is the number of baboons.
+    - 2 is the x and y coordinates of the baboon.
+For example baboons_trajectory[t, i, :] is the position of baboon i at time t.
 
-    def __str__(self):
-        return f"Baboon({self._coordinates[0]}, {self._coordinates[1]}, color={self.color})"
-
-    def __repr__(self):
-        return f"Baboon({self._coordinates[0]}, {self._coordinates[1]}, color={self.color})"
+colors: List[str]: List of colors for each baboon. (List of length M)
+"""
